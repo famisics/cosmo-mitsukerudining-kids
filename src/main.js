161f6bba49203167ae7ba6de54f5831b4d2feb17
famisics <stnfy.com@gmail.com>
2023-11-13 23:35:41 +0900

    パッケージアップデート

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import VueGtag from "vue-gtag";
 import { createPinia } from 'pinia'
 import VueSweetalert2 from 'vue-sweetalert2'
 
@@ -11,5 +12,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueSweetalert2)
-
+app.use(VueGtag, {
+  config: { id: "G-CJGRVF45YV" }
+})
 app.mount('#app')
